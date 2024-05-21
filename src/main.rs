5052a9405a81eafe10b0a3353f7ff4e1d69d2edc
Duc Huong <duchuong007@gmail.com>
2024-05-21 22:31:51 +0700

    feat(trace): add breakpoint handler

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,15 @@ use rust_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    rust_os::init(); // new
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3(); // new
+
     #[cfg(test)]
     test_main();
+
+    println!("It did not crash!");
 
     loop {}
 }
